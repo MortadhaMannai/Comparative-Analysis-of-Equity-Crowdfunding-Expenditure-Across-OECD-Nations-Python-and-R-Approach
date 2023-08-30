ec092ebfd3ca0f64a148d9509be67a268d6a7fe0
Mortadha Mannai <93622509+MortadhaMannai@users.noreply.github.com>
2023-08-30 02:40:19 +0100

    Add files via upload

---
+++ knn.R
@@ -0,0 +1,73 @@
+# K-Nearest Neighbors (K-NN)
+
+# Importing the dataset - column order is important! Dummy variable column 
+# should be the last
+dataset = read.csv('airbnb_warsaw.csv', sep = ';')
+dataset = dataset[c(8,10, 5)]
+
+# Encoding the target feature as factor
+dataset$good_rating = factor(dataset$good_rating, levels = c(0, 1))
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$good_rating, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+training_set[, 1:2] = scale(training_set[, 1:2])
+test_set[, 1:2] = scale(test_set[, 1:2])
+
+# Fitting K-NN to the Training set and Predicting the Test set results
+library(class)
+y_pred = knn(train = training_set[, -3],
+             test = test_set[, -3],
+             cl = training_set[, 3],
+             k = 5,
+             prob = TRUE)
+
+#show me the first 10 variabes
+head(y_pred, n=10)
+
+# Making the Confusion Matrix
+cm = table(test_set[, 3], y_pred)
+
+# Visualising the Training set results
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('accommodates', 'price')
+y_grid = knn(train = training_set[, -3], 
+             test = grid_set, 
+             cl = training_set[, 3], 
+             k = 5)
+plot(set[, -3],
+     main = 'K-NN (Training set)',
+     xlab = 'accommodates', ylab = 'price',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+# Visualising the Test set results
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('accommodates', 'price')
+y_grid = knn(train = training_set[, -3], 
+             test = grid_set, 
+             cl = training_set[, 3], 
+             k = 5)
+plot(set[, -3],
+     main = 'K-NN (Test set)',
+     xlab = 'accommodates', ylab = 'price',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
